3c0a715af4161cf596567870d1c96da88799d8fe
Gintautas Petkevicius <g.petkevicius@inbox.lt>
2019-07-09 13:28:59 +0300

    veikia be išsaugojimo failuose

--- SkaiciaiTekste.java
+++ SkaiciaiTekste.java
@@ -4,69 +4,53 @@ import java.io.FileReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+import java.util.ArrayList; 
+
+
 public class SkaiciaiTekste {
 	
-	public static boolean yraSkaitmuo ( String simbolis ) {
+	public static boolean priklausoAibei ( String simbolis, String[] aibe ) {
 		
-		String[] skaitmenys = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+		boolean priklauso = false;
 		
-		boolean yra_skaitmuo = false;
+		for (int i = 0; i < aibe.length; i++ ) {
 		
-		for (int i = 0; i < 10; i++ ) {
-		
-			if ( simbolis.equals ( skaitmenys [ i ] ) ) {
+			if ( simbolis.equals ( aibe [ i ] ) ) {
 			
-				yra_skaitmuo = true;
+				priklauso = true;
 			}
 		}
-		return yra_skaitmuo;
+		return priklauso;
 	}
-	
-	public static boolean yraDalisSkaiciaus ( String simbolis ) {
-
-		String[] dalys_skaiciaus = { ".", ",", "-", "e", "E", "+", "/", "%" };
-
-		boolean yra_dalis_skaiciaus = false;
-		
-		for (int i = 0; i < dalys_skaiciaus.length; i++ ) {
-		
-			if ( simbolis.equals ( dalys_skaiciaus [ i ] ) ) {
-			
-				yra_dalis_skaiciaus = true;
-			}
-		}
-		return yra_dalis_skaiciaus;
-	}
-	
-	public static boolean yraZodzioPabaiga ( String simbolis ) {
-
-		String[] zodzio_pabaigos = { ".", ",", " ", "\n", ";", ":" };
-
-		boolean yra_zodzio_pabaiga = false;
-		
-		for (int i = 0; i < zodzio_pabaigos.length; i++ ) {
-		
-			if ( simbolis.equals ( zodzio_pabaigos [ i ] ) ) {
-			
-				yra_zodzio_pabaiga = true;
-			}
-		}
-		return yra_zodzio_pabaiga;
-	}	
-	
 		
 	public static void main(String[] args) throws Exception {
 	   
 		String thisLine = null;
 		
+		String[] skaitmenys = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+		String[] dalys_skaiciaus = { ".", ",", "-", "e", "E", "+", "/", "%" };
+		String[] zodzio_pabaigos = { ".", ",", " ", "\n", ";", ":" };
+		String[] ne_zodzio_dalis = { ")", ",", ";", "." , "/" };
+		
+		// pagal -------------> https://www.geeksforgeeks.org/java-util-arraylist-indexof-java/
+		ArrayList<String> vnt = new ArrayList<String>(1000) ;
+		ArrayList<String> anti_vnt = new ArrayList<String>(1000) ;
+		
 		try {
 																									// open input stream test.txt for reading purpose.
 			BufferedReader br = new BufferedReader( new FileReader( "tekstas.txt" ) );
+			BufferedReader in_kb = new BufferedReader( new InputStreamReader ( System.in ) );
+			
+			BufferedReader vnt_file = new BufferedReader ( new FileReader ( "vnt.csv" ) );
+			
+			
+			BufferedReader anti_vnt_file = new BufferedReader ( new FileReader ( "anti_vnt.csv" ) );
 			
 			System.out.println ( "duomenų failo turinys:" );
 			
 			String simb;
 			String[] skaiciai = new String [ 1000 ];
+			String[] vnt_po_skaiciu = new String [ 1000 ];
 																									// double[] skaiciai = new double[1000];
 			int kiekis_skaiciu = 0; 
 
@@ -84,24 +68,32 @@ public class SkaiciaiTekste {
 					simb =  thisLine.substring( i, i+1 );
 				
 					if ( 
-							yraSkaitmuo ( simb ) 
+							priklausoAibei ( simb, skaitmenys ) 
 						|| 
 							( 
 									pries_tai_buvo_skaitmuo  
 								&& 
-									yraDalisSkaiciaus ( simb )
+									priklausoAibei ( simb, dalys_skaiciaus )
 							) 
 						||
 							yra_zodis_po_skaitmens
 					) {
 						
 						if ( pries_tai_buvo_skaitmuo || yra_zodis_po_skaitmens ) { 				//  ------
+							
+							if ( yra_zodis_po_skaitmens ) {
+								
+								vnt_po_skaiciu [ kiekis_skaiciu - 1 ] += simb;  
+								
+							} else {
 						
-							skaiciai [ kiekis_skaiciu - 1 ] += simb;
+								skaiciai [ kiekis_skaiciu - 1 ] += simb;
+							}
 							
 						} else {
 							
 							skaiciai [ kiekis_skaiciu ] = simb;
+							vnt_po_skaiciu [ kiekis_skaiciu ] = "";
 							kiekis_skaiciu++;
 						}
 						
@@ -111,7 +103,7 @@ public class SkaiciaiTekste {
 							
 						} else {
 						
-							if ( yraZodzioPabaiga ( simb ) ) {
+							if ( priklausoAibei ( simb, zodzio_pabaigos ) ) {
 								
 								yra_zodis_po_skaitmens = false;
 							}
@@ -120,9 +112,13 @@ public class SkaiciaiTekste {
 					} else {
 						
 						if ( pries_tai_buvo_skaitmuo ) {
+							
+							if ( ! priklausoAibei ( simb, ne_zodzio_dalis ) ) {
 						
-							yra_zodis_po_skaitmens = true;
-							skaiciai [ kiekis_skaiciu - 1 ] += simb;
+								yra_zodis_po_skaitmens = true;
+								// skaiciai [ kiekis_skaiciu - 1 ] += simb;
+								vnt_po_skaiciu [ kiekis_skaiciu - 1 ] += simb;
+							}
 						}
 						pries_tai_buvo_skaitmuo = false;
 					}
@@ -131,10 +127,38 @@ public class SkaiciaiTekste {
 			System.out.println ( "viso skaiciu: " + kiekis_skaiciu );
 			
 			for ( int i = 0; i < kiekis_skaiciu; i++ ) {
-			
-				System.out.println ( " " + skaiciai [ i ] );
+				
+				System.out.println ( " " + skaiciai [ i ] + " " + vnt_po_skaiciu [ i ] );
+				
+				if ( 
+						( vnt.indexOf ( vnt_po_skaiciu [ i ].trim() )  == -1 ) 
+					&&  
+						(  ! vnt_po_skaiciu [ i ].trim().equals( "" ) ) 
+					&&
+						( anti_vnt.indexOf ( vnt_po_skaiciu [ i ].trim() ) == -1 )
+				) {
+				
+					System.out.println ( "įtraukti į mat. vnt.? " );
+					String ats = in_kb.readLine();
+					
+					if ( ats.equals( "" ) ) {
+						
+						vnt.add ( vnt_po_skaiciu [ i ].trim() );
+						
+					} else {
+					
+						anti_vnt.add ( vnt_po_skaiciu [ i ].trim() );
+					}
+				}
 			}
+			
 			System.out.println();
+			
+			for ( String value : vnt ) { 
+				
+				System.out.print( value ); 
+				System.out.print( " " ); 
+			}
 			
 		} catch( Exception e ) {
 			
